1f197d6d855e77b9c696c203914bfa23a3dbd177
archanVadgama <archan.vadgama@simformsolutions.com>
2025-03-11 18:53:00 +0530

    stream, buffer and pipe

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # node-practice
+
+This is the example of Stream, Buffer and Pipe this is used to Stream video dynamically from the server
+
+## Installation
+- Clone the repo in local 
+- Now upload video the video in location "assets/video/{your_video}"
+- Now change name of video file to "sample_video.mp4"
+- Then use below command
+```
+npm install
+npm start
+```
+- Now open link and enjoy!
---
+++ assets/js/index.js
@@ -0,0 +1,58 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    fs.readFile('index.html', (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Internal Server Error');
+        return;
+      }
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    });
+  } else if (req.url === '/assets/video/sample_video.mp4') {
+    const videoPath = 'assets/video/sample_video.mp4';
+    console.log("req.url "+req.url);
+    console.log('videoPath '+videoPath);
+    fs.stat(videoPath, (err, stats) => {
+      if (err) {
+        res.writeHead(404);
+        res.end('Video Not Found');
+        return;
+      }
+      const range = req.headers.range;
+      if (range) {
+        const parts = range.replace(/bytes=/, '').split('-');
+        const start = parseInt(parts[0], 10);
+        const end = parts[1] ? parseInt(parts[1], 10) : stats.size - 1;
+        const chunkSize = (end - start) + 1;
+        const file = fs.createReadStream(videoPath, { start, end });
+        const head = {
+          'Content-Range': `bytes ${start}-${end}/${stats.size}`,
+          'Accept-Ranges': 'bytes',
+          'Content-Length': chunkSize,
+          'Content-Type': 'video/mp4',
+        };
+        res.writeHead(206, head);
+        file.pipe(res);
+      } else {
+        const head = {
+          'Content-Length': stats.size,
+          'Content-Type': 'video/mp4',
+        };
+        res.writeHead(200, head);
+        fs.createReadStream(videoPath).pipe(res);
+      }
+    });
+  } else {
+    res.writeHead(404);
+    res.end('Not Found');
+  }
+});
+
+server.listen(8000, () => {
+  console.log('Server is running on http://localhost:8000');
+});
